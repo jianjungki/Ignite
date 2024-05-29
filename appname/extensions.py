e098b848c0e1cab41b458220901397f902e8531f
jianjun <1188643+jianjungki@users.noreply.github.com>
2024-05-29 03:20:15 +0000

    add redis cache

--- appname/extensions.py
+++ appname/extensions.py
@@ -48,7 +48,10 @@ token = Token()
 rq2 = RQ()
 admin = AdminDashboard()
 mail = Mail()
-limiter = Limiter(key_func=get_remote_address)
+limiter = Limiter(
+    key_func=get_remote_address,
+    storage_uri=CACHE_REDIS_URL
+)
 stripe = Stripe()
 sentry = Sentry()
 hashids = HashIds()
